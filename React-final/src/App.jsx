5841734f238b2ecd6b19464457f35d4350826eb1
Andre-Lucho <andretlucho@gmail.com>
2025-11-21 09:50:49 -0300

    [feat]Implementações iniciais Header e Footer + rotas\

--- React-final/src/App.jsx
+++ React-final/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import Api from './teste-api/Api';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ React-final/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/login'}>Login / Criar</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
