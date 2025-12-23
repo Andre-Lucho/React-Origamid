851eed0ac7ae74474d65ffc219a53989d2f420ed
Andre-Lucho <andretlucho@gmail.com>
2025-12-23 18:38:53 -0300

    [feat]begining user routes

--- React-final/src/components/User/User.jsx
+++ React-final/src/components/User/User.jsx
@@ -1,9 +1,18 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import UserHeader from './UserHeader';
+import Feed from '../Feed';
+import UserMetrics from './UserMetrics';
+import UserPhotoPost from './UserPhotoPost';
 
 const User = () => {
   return (
     <section>
-      <div>Usuário</div>
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/photopost" element={<UserPhotoPost />} />
+        <Route path="/metrics" element={<UserMetrics />} />
+      </Routes>
     </section>
   );
 };
